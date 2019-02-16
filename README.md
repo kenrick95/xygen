054c1b029cacb737755afef57d416eca6533c8e4
Kenrick <kenrick95@gmail.com>
2019-02-16 22:33:24 +0800

    Init commit, putting ideas in

---
+++ README.md
@@ -0,0 +1,14 @@
+# xygen
+
+Coldplay X&Y art generator
+
+Modern browser is required. IE is not supported. Tested on Firefox 65.
+
+Vanilla JS (ES6+).
+
+Idea
+
+Input text, display as baudot code, in Coldplay's X&Y style.
+
+Inspiration: put a link...
+
---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+(function () {
+    function main () {
+        const canvas = document.getElementById('canvas');
+        const context = canvas.getContext('2d');
+    
+        // get input
+        // transform to baudot code
+        // draw x&y
+    }
+    
+    document.addEventListener('DOMContentLoaded', main);F
+})();
